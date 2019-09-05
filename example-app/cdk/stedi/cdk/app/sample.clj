48de0b9bc5a57af7d49061f1f1a76993885bb7d4
Tyler van Hensbergen <tyler@stedi.com>
2019-09-05 10:53:14 -0600

    Update examples

--- example-app/cdk/stedi/cdk/app/sample.clj
+++ example-app/cdk/stedi/cdk/app/sample.clj
@@ -20,19 +20,56 @@
   (app-stack this "test"))
 
 (comment
-  (Function stack "my-fn" {}) ;; constructor
-  (:role function)            ;; instance prop
-  (:JAVA_8 Runtime)           ;; static prop
-  (Function/fromFunctionArn)  ;; static method
-  (Function/toString this)    ;; instance method
-  Effect/ALLOW                ;; enum
-  )
+  ;; Instantiation:
 
-#_(def app
-  (let [app   (app/create)
-        stack (stack/create app "foobar")]
-    (function/create stack "fn"
-                     {:code    (asset-code/create "./src")
-                      :handler "foo.bar"
-                      :runtime (runtime/JAVA_8)})
-    app))
+  ;; old
+  (cdk/require ["@aws-cdk/aws-lambda" lambda])
+  (lambda/Function :cdk/create stack "myfn" {})
+
+  ;; new
+  (cdk/import ("@aws-cdk/aws-lambda" Function))
+  (Function stack "my-fn" {})
+
+  ;; Instance Properties (unchanged):
+  (:role function)
+
+  ;; Static Properties:
+
+  ;; old
+  (cdk/require ["@aws-cdk/aws-lambda" lambda])
+  (:JAVA_8 lambda/Runtime)
+
+  ;; new
+  (cdk/import ("@aws-cdk/aws-lambda" Runtime))
+  (:JAVA_8 Runtime)
+
+
+  ;; Static Methods:
+
+  ;; old
+  (cdk/require ["@aws-cdk/core" cdk-core])
+  (cdk-core/Duration :minutes 5)
+
+  ;; new
+  (cdk/import ("@aws-cdk/core" Duration))
+  (Duration/minutes 5)
+
+  ;; Instance Methods:
+
+  ;; old
+  (function :addEnvironment "key" "value")
+
+  ;; new
+  (cdk/import ("@aws-cdk/aws-lambda" Function))
+  (Function/addEnvironment function "key" "value")
+
+  ;; Enums:
+
+  ;; old
+  (cdk/require ["@aws-cdk/aws-iam" iam])
+  (iam/Effect :cdk/enum :ALLOW)
+
+  ;; new
+  (cdk/import ("@aws-cdk/aws-iam" Effect))
+  Effect/ALLOW
+  )
