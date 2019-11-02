0e68ce0a426f44f66de90f276a5444f631a04b31
Eno Compton <eno@stedi.com>
2019-11-02 08:43:54 -0600

    Remove stedi/cdk/lambda NS.
    The lambda namespace is an orthogonal concern of cdk-clj. Consumers should be
    defining the "function" in their own code.

--- example-app/cdk/stedi/cdk/app/sample.clj
+++ example-app/cdk/stedi/cdk/app/sample.clj
@@ -1,21 +1,33 @@
 (ns stedi.cdk.app.sample
-  (:require [stedi.cdk :as cdk]
-            [stedi.cdk.lambda :as lambda]
-            [stedi.app.sample :as sample-app]))
+  (:require [clojure.java.io :as io]
+            [stedi.app.sample :as sample-app]
+            [stedi.cdk :as cdk]
+            [stedi.lambda.build :as lambda-build]))
 
-(cdk/import ["@aws-cdk/core" App Stack]
+(cdk/import ["@aws-cdk/core" App Duration Stack]
             ["@aws-cdk/aws-apigateway" LambdaRestApi]
-            ["@aws-cdk/aws-lambda" Tracing])
+            ["@aws-cdk/aws-lambda" Code Function Runtime Tracing])
 
 (def app (App {}))
 
 (def stack (Stack app "DevStack"))
 
-(def function
-  (lambda/fn-from-var stack "function" #'sample-app/handler
-                      {:tracing Tracing/ACTIVE}))
+(defn function
+  [scope id]
+  (let [entrypoint (-> #'sample-app/handler (symbol) (str))
+        jar-path   (lambda-build/target-zip entrypoint)]
+    (when-not (.exists (io/file jar-path))
+      (throw (Exception. (format (str "Could not find lambda jar in expected location %s.\n"
+                                      "Did you add stedi/lambda to :deps and run `clj -m stedi.lambda.build`?")
+                                 jar-path))))
+    (Function scope id {:handler    "stedi.lambda.Entrypoint::handler"
+                        :runtime    (:JAVA_8 Runtime)
+                        :code       (Code/fromAsset jar-path)
+                        :memorySize 512
+                        :timeout    (Duration/seconds 30)
+                        :tracing    Tracing/ACTIVE})))
 
 (def api
   (LambdaRestApi stack "api"
-                 {:handler       function
+                 {:handler       (function stack "Function")
                   :deployOptions {:tracingEnabled true}}))
