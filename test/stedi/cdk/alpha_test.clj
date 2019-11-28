51369707fd936a27b56b9ded12fb73b11909d66e
Eno Compton <eno@stedi.com>
2019-11-28 10:14:24 -0700

    Back-fill tests for browse and remove duplicate invocation.
    This commit adds tests in service of eventually resolving #53. Likewise, it
    removes a mistaken duplicate invocation where on call to browse would result in
    two calls to browse-url.

--- test/stedi/cdk/alpha_test.clj
+++ test/stedi/cdk/alpha_test.clj
@@ -25,3 +25,46 @@
       (is (StringParameter/grantRead
             (StringParameter/fromStringParameterName s "param" "param-name")
             (Function/fromFunctionArn s "fn" "function-arn"))))))
+
+(deftest browse-test
+  (testing "opening documentation with no arguments"
+    (let [spy (atom [])]
+      (with-redefs [clojure.java.browse/browse-url
+                    (fn [url] (swap! spy conj url))]
+        (cdk/browse)
+        (is (= ["https://docs.aws.amazon.com/cdk/api/latest"] @spy)))))
+  (testing "opening module documentation with full path"
+    (let [spy (atom [])]
+      (with-redefs [clojure.java.browse/browse-url
+                    (fn [url] (swap! spy conj url))]
+        (cdk/browse "@aws-cdk/core")
+        (is (= ["https://docs.aws.amazon.com/cdk/api/latest/docs/core-readme.html"]
+               @spy)))))
+  (testing "opening module documentation for `core`"
+    (let [spy (atom [])]
+      (with-redefs [clojure.java.browse/browse-url
+                    (fn [url] (swap! spy conj url))]
+        (cdk/browse "core")
+        (is (= ["https://docs.aws.amazon.com/cdk/api/latest/docs/core-readme.html"]
+               @spy)))))
+  (testing "opening fully qualified documentation for a class"
+    (let [spy (atom [])]
+      (with-redefs [clojure.java.browse/browse-url
+                    (fn [url] (swap! spy conj url))]
+        (cdk/browse "@aws-cdk/core.Stack")
+        (is (= ["https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.Stack.html"]
+               @spy)))))
+  (testing "opening documentation for a class"
+    (let [spy (atom [])]
+      (with-redefs [clojure.java.browse/browse-url
+                    (fn [url] (swap! spy conj url))]
+        (cdk/browse Stack)
+        (is (= ["https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.Stack.html"]
+               @spy)))))
+  (testing "opening documentation for an instance"
+    (let [spy (atom [])]
+      (with-redefs [clojure.java.browse/browse-url
+                    (fn [url] (swap! spy conj url))]
+        (cdk/browse (Stack))
+        (is (= ["https://docs.aws.amazon.com/cdk/api/latest/docs/@aws-cdk_core.Stack.html"]
+               @spy))))))
